939fefb402796a5619b362641e4a0f415b39b5a2
mason long <mwanger32@gmail.com>
2025-01-26 20:02:09 -0500

    asdf

--- knowledgemap/map.py
+++ knowledgemap/map.py
@@ -1,5 +1,7 @@
 import numpy as np
 import os
+import glob
+
 import networkx as nx
 from pyvis.network import Network
 from skimage.color import rgb2lab, lab2rgb
@@ -13,11 +15,11 @@ def interpolate_colors(nodes):
     return to_hex(avg_color_rgb)
 
 class Node:
-    def __init__(self, name, base_size=0, parents=[],
+    def __init__(self, name, base_radius=0, parents=[],
                  color=None, type='document', description="",
                  link = "", review_flag=False):
         self.name = name
-        self.base_size = base_size
+        self.base_area = base_radius**2
         self.parents = parents
         if color is None:
             if len(parents) > 0:
@@ -36,11 +38,10 @@ class Node:
             
     def add_child(self, child):
         self.children.append(child)
-    
-    
-    def compute_size(self):
+
+    def compute_area(self):
         descendants = self._collect_unique_descendants()
-        return sum(node.base_size for node in descendants)
+        return sum(node.base_area for node in descendants)
     
     def _collect_unique_descendants(self):
         descendants = set()
@@ -55,11 +56,11 @@ class KnowledgeMap:
     def __init__(self):
         self.node_dict = {}
         
-    def add_node(self, name, base_size=0, parent_names=[], color=None,
+    def add_node(self, name, base_radius=0, parent_names=[], color=None,
                 type='document', description="", link="", review_flag=False):
         parents = [self.node_dict[parent] for parent in parent_names]
         self.node_dict[name] = Node(name,
-                            base_size=base_size,
+                            base_radius=base_radius,
                             parents=parents,
                             color=color,
                             type=type,
@@ -83,12 +84,14 @@ class KnowledgeMap:
             node = self.node_dict[g_node]
             label = f"“{node.name}”" if node.type in ['document', 'book', 'class', 'paper', 'blog post', 'chapter'] else node.name
             
-            adjusted_size = node.compute_size() * 1
+            adjusted_radius = np.sqrt(node.compute_area()) * 1
             
             if node.link:
                 if node.link == "auto":
                     pluralized_type = node.type + 's' if node.type not in ['class'] else node.type + 'es'
                     file_location = os.path.join("Notes", pluralized_type, node.name + ".notes")
+                    if not os.path.exists(file_location):
+                        raise Error(f"File Not Found:\t{file_location}")
                 else:
                     file_location = node.link
                               
@@ -105,8 +108,8 @@ class KnowledgeMap:
             node_attrs = {
                 "label": label,
                 "title": title,
-                "size": max(adjusted_size, 5),
-                "mass": adjusted_size / 100,
+                "size": max(adjusted_radius, 5),
+                "mass": adjusted_radius**2 / 100,
                 "fixed": False
             }
             
@@ -176,7 +179,13 @@ class KnowledgeMap:
             }
         }
         """
-        
+
+        #with open("template.html") as f:
+        #    custom_template = f.read()
+
+        # 2) Tell PyVis to use that template
+        #net.set_template(custom_template)
+
         net.set_options(options_string)
         net.write_html("index.html")
 
@@ -189,65 +198,93 @@ if __name__=="__main__":
     M = KnowledgeMap()
 
 
-    # size 7 = size of a paper
+    # radius 7 = one paper
+    # radius 10 = UDL Chapter, Chain Rule, Gradients, Adam, UDL 
     # Math
     M.add_node("Math", type='concept', color="#C41E3A")
-    M.add_node("Statistics", type='concept',  parent_names=["Math"])
+    M.add_node("Statistics", type='concept',  parent_names=["Math"], color='#FF6F20') #FFBF00
     M.add_node("Information Theory", type='concept', parent_names=["Math"])
     M.add_node("Linear Algebra", type='concept',  parent_names=["Math"])
     M.add_node("Calculus", type='concept',  parent_names=["Math"])
     M.add_node("Optimization", type='concept', parent_names=["Statistics"])
     
-    M.add_node("Momentum, RMSProp, Adam", type='concept', parent_names=["Optimization"], base_size=10, link='auto')
-    M.add_node("Gradients", type='concept',  parent_names=["Linear Algebra", "Calculus"], base_size=10, link="auto")
-    M.add_node("Chain Rule", type='concept',  parent_names=["Gradients"], base_size=10, link="auto")
-    M.add_node("Functions", type='concept',  parent_names=["Math"], base_size=1, link="auto")
+    M.add_node("Momentum, RMSProp, Adam", type='concept', parent_names=["Optimization"], base_radius=10, link='auto')
+    M.add_node("Gradients", type='concept',  parent_names=["Calculus", "Linear Algebra"], base_radius=10, link="auto")
+    M.add_node("Chain Rule", type='concept',  parent_names=["Gradients"], base_radius=10, link="auto")
+
+    M.add_node("Functions", type='concept',  parent_names=["Math"], base_radius=1, link="auto")
     
-    M.add_node("A Brief Introduction To Information", type='blog post', parent_names=["Information Theory"], link="auto", base_size=2)
+    M.add_node("A Brief Introduction To Information", type='blog post', parent_names=["Information Theory"], link="auto", base_radius=2)
 
     M.add_node("Deep Learning Chapter 3", type='chapter', parent_names=["Information Theory"])
-    M.add_node("KL Divergence", type='concept', parent_names=["Deep Learning Chapter 3"], base_size=5, link="auto")
-    M.add_node("Entropy", type='concept', parent_names=["Deep Learning Chapter 3"], base_size=5, link="auto")
-    M.add_node("Info Theory Basics", type='concept', parent_names=["Deep Learning Chapter 3"], base_size=5  , link="auto")
+    M.add_node("KL Divergence", type='concept', parent_names=["Information Theory"], base_radius=5, link="auto")
+    M.add_node("Six Interpretations of KL Divergence", type='blog post', parent_names=["KL Divergence"], base_radius=5, link="auto")
+    M.add_node("Entropy", type='concept', parent_names=["Deep Learning Chapter 3"], base_radius=5, link="auto")
+    M.add_node("Cross Entropy", type='concept', parent_names=["Deep Learning Chapter 3"], base_radius=5, link="auto")
+
+    M.add_node("Info Theory Basics", type='concept', parent_names=["Deep Learning Chapter 3"], base_radius=5  , link="auto")
+    M.add_node("Random Variables", type='concept', parent_names=["Statistics"], base_radius=10, link='auto')
+    M.add_node("Bayes", type='concept', parent_names=["Statistics"], base_radius=10, link='auto')
+    M.add_node("Conditional Independence", type='concept', parent_names=["Statistics"], base_radius=5, link='auto')
+
 
 
     # Deep Learning
     M.add_node("Deep Learning", type='concept', color="#0000FF")
+    
+    # Software
+    M.add_node("Software", type='concept', color="#212129")
+    M.add_node("PyTorch", type='software', parent_names=["Software", "Deep Learning"], base_radius=10, link='auto')
+
+
 
     # Activation Functions
     M.add_node("Activation Functions", type='concept', parent_names=["Deep Learning"])
-    M.add_node("Pocketed Activations", type='concept', parent_names=["Activation Functions"], base_size=2)
+    M.add_node("Pocketed Activations", type='concept', parent_names=["Activation Functions"], base_radius=2, link='auto')
+    M.add_node("Gated Activations", type='concept', parent_names=["Activation Functions"], base_radius=2, link='auto')
 
-    # Generative Modeling
-    M.add_node("Generative Modeling", type='concept', color="#FFA500", parent_names=["Deep Learning"])
 
-    # Vision
-    M.add_node("Vision", type='concept', color="#79443B")
-    M.add_node("VAR", type='paper', parent_names=["Vision", "Generative Modeling"], base_size=7, link='auto')
 
 
     # UDL Textbook
     M.add_node("Understanding Deep Learning", type='book', parent_names=["Deep Learning"])
-    M.add_node("MLP Interpretation - UDL", type='chapter', parent_names=["Understanding Deep Learning"], base_size=10, description="Last Recall: 10/24/24", link="auto")
-    
-    M.add_node("Loss Functions - UDL", type='chapter', parent_names=["Understanding Deep Learning"], base_size=10, link="auto")
-    M.add_node("Optimization - UDL", type='chapter', parent_names=["Understanding Deep Learning", "Optimization"], base_size=10, link="auto")
-    
-    
-    # Diffusion 
+    M.add_node("MLP Interpretation - UDL", type='chapter', parent_names=["Understanding Deep Learning"], base_radius=10, description="Last Recall: 10/24/24", link="auto")
+    M.add_node("Loss Functions - UDL", type='chapter', parent_names=["Understanding Deep Learning"], base_radius=10, link="auto")
+
+    # Generative Modeling FFBF00
+    M.add_node("Generative Modeling", type='concept', color="#FFD900", parent_names=["Deep Learning"]) #FFA500
+    M.add_node("VAEs - UDL", type='chapter', parent_names=["Understanding Deep Learning", "Generative Modeling"], base_radius=10, link='auto')
+    M.add_node("ELBO", type='concept', parent_names=["Optimization", "VAEs - UDL"], base_radius=4, link='auto')
+    M.add_node("Jensens Inequality", type='concept', parent_names=["ELBO"], base_radius=4, link='auto')
+    M.add_node("Optimization - UDL", type='chapter', parent_names=["Understanding Deep Learning", "Optimization"], base_radius=10, link="auto")
+
     M.add_node("Diffusion Models", type='concept', parent_names=["Generative Modeling"])
-    M.add_node("Understanding Diffusion Models: A Unified Perspective", type='blog post', parent_names=["Diffusion Models"], base_size=20, description="Last Recall: 7/14/24", review_flag=True)
+    M.add_node("Diffusion Models - UDL", type='chapter', parent_names=["Understanding Deep Learning", "Diffusion Models"], base_radius=10, link='auto')
+    M.add_node("Noise Schedule", type='concept', parent_names=["Diffusion Models - UDL"], base_radius=5, link='auto')
+
+    M.add_node("Understanding Diffusion Models: A Unified Perspective", type='blog post', parent_names=["Diffusion Models"], base_radius=20, description="Last Recall: 7/14/24", review_flag=True)
+
+    # Audio
+    M.add_node("Audio", type='concept', color="#3FFF57")
+    M.add_node("DiffWave", type='paper', parent_names=["Audio", "Diffusion Models"], base_radius=7, link='auto')
+    M.add_node("DAC", type='paper', parent_names=["Audio"], base_radius=7, link='auto')
+
+
+    # Vision
+    M.add_node("Vision", type='concept', color="#79443B")
+    M.add_node("VAR", type='paper', parent_names=["Vision", "Generative Modeling"], base_radius=7, link='auto')
+    M.add_node("Gaussian Splatting", type='paper', parent_names=["Vision"], base_radius=7, link='auto')
 
 
     # Language Modeling
     M.add_node("Language Modeling", type='concept', parent_names=["Deep Learning"], color="#00FF00")
     M.add_node("Language Modeling from Scratch", type='class', parent_names=["Language Modeling"], description="Last Recall: 6/14/24", link="auto")
-    M.add_node("Transformers", type='concept', parent_names=["Language Modeling from Scratch"], description="Last Recall: 6/14/24", link="auto", base_size=10)
+    M.add_node("Transformers", type='concept', parent_names=["Language Modeling from Scratch"], description="Last Recall: 6/14/24", link="auto", base_radius=10)
 
 
     # Papers
     M.add_node("Interpretability", type='concept', parent_names=["Deep Learning"])
-    M.add_node("Concept Activation Vectors", type='paper', parent_names=["Interpretability"], base_size=7, link="auto")
+    M.add_node("Concept Activation Vectors", type='paper', parent_names=["Interpretability"], base_radius=7, link="auto")
 
 
     # Fine Tuning
@@ -255,16 +292,22 @@ if __name__=="__main__":
 
     # Signal Processing
     M.add_node("Signal Processing", type='concept',color="#a8326f")
-    M.add_node("DDSP", type='concept',  parent_names=["Signal Processing", "Deep Learning"], base_size=7)
-    M.add_node("PQMF", type='concept',  parent_names=["Signal Processing"], base_size=7, link="auto")
-    M.add_node("Downsampling and Stretching", type='concept',  parent_names=["Signal Processing"], base_size=7, link="auto")
-    M.add_node("Convolution", type='concept',  parent_names=["Signal Processing"], base_size=7, link="auto")
-    M.add_node("Transpose Convolution", type='concept',  parent_names=["Signal Processing"], base_size=7, link="auto")
+    M.add_node("DDSP", type='concept',  parent_names=["Signal Processing", "Deep Learning"], base_radius=7)
+    M.add_node("PQMF", type='concept',  parent_names=["Signal Processing"], base_radius=7, link="auto")
+    M.add_node("Downsampling and Stretching", type='concept',  parent_names=["Signal Processing"], base_radius=7, link="auto")
+    M.add_node("Convolution", type='concept',  parent_names=["Signal Processing"], base_radius=7, link="auto")
+    M.add_node("Transpose Convolution", type='concept',  parent_names=["Signal Processing"], base_radius=7, link="auto")
     
     # RL
     M.add_node("Reinforcement Learning", type='concept', color="#808080")
-    M.add_node("ReaLChords", type='paper', parent_names=["Reinforcement Learning"], base_size=7, description="Last Recall: 9/25/24")
-    M.add_node("CS 285", type='class',  parent_names=["Reinforcement Learning"], base_size=10, description="Last Recall: 9/25/24")
+    M.add_node("ReaLChords", type='paper', parent_names=["Reinforcement Learning"], base_radius=7, description="Last Recall: 9/25/24")
+    M.add_node("CS 285", type='class',  parent_names=["Reinforcement Learning"], base_radius=10, description="Last Recall: 9/25/24")
     M.render()
+    
+    
+    list_of_notes_documents = [os.path.splitext(os.path.basename(path))[0] for path in glob.glob("Notes/*/*.notes")]
+    unassigned_notes = [name for name in list_of_notes_documents if name not in M.node_dict.keys()]    
+    print("Unassigned Notes")
+    print(unassigned_notes)
     
 
